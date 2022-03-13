734f553059b9d7c130dfc199d80e80045cc30f55
wlsgh7608 <wlsgh7608@naver.com>
2022-03-13 16:59:24 +0900

    bfs & dfs / 실버2 / 20분 (주석 추가)

--- dfs&bfs/baekjoon/1260.py
+++ dfs&bfs/baekjoon/1260.py
@@ -6,6 +6,9 @@ from collections import deque
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 def dfs(G,visited,vertex):
+    """
+    dfs는 스택 자료구조(재귀) 이용
+    """
     visited[vertex] = True
     print(vertex+1, end = " ")
     for p in G[vertex]:
@@ -13,6 +16,9 @@ def dfs(G,visited,vertex):
             dfs(G,visited,p)
 
 def bfs(G,visited,vertex):
+    """
+    bfs는 큐 자료구조 이용
+    """
     Q = deque()
     Q.append(vertex)
     visited[vertex] = True
@@ -23,8 +29,10 @@ def bfs(G,visited,vertex):
             if not visited[p]:
                 Q.append(p)
                 visited[p] = True
+
 N, M, V = map(int,input().split())
 G = [[]for _ in range(N)]
+# 인접 리스트 생성
 for _ in range(M):
     s,e = map(int,input().split())
     G[s-1].append(e-1)
