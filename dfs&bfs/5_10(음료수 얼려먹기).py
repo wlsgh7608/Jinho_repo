a4256cfe695d58c89c5da4b36b2352acbceb221f
wlsgh7608 <wlsgh7608@naver.com>
2022-03-13 16:41:33 +0900

    bfs&dfs 책 문제

--- dfs&bfs/5_10(음료수 얼려먹기).py
+++ dfs&bfs/5_10(음료수 얼려먹기).py
@@ -1,3 +1,16 @@
+def dfs(x,y):
+    dx_list = [-1,1,0,0]
+    dy_list = [0,0,1,-1]
+    G[i][j] = 1
+
+    for dx,dy in zip(dx_list,dy_list):
+        nx = x + dx
+        ny = y + dy
+        if 0<=nx<N and 0<=ny<M and G[nx][ny] == 0:
+            G[nx][ny] = 1
+            dfs(nx,ny)
+
+
 import sys
 input = sys.stdin.readline
 N,M = map(int,input().split())
@@ -5,7 +18,13 @@ G = []
 for _ in range(N):
     G.append(list(map(int,input().rstrip())))
     
-print(G)
+cnt = 0
+for i in range(N):
+    for j in range(M):
+        if G[i][j] == 0:
+            dfs(i,j)
+            cnt+=1
+print(cnt)
 """
 input
 4 5 
