9aecd033abfb90913fa67b8e12dfb0fb16ff5953
wlsgh7608 <wlsgh7608@naver.com>
2022-03-17 22:44:17 +0900

    수 정렬하기 2 / 실버 5 / 3분

--- sort/baekjoon/2751.py
+++ sort/baekjoon/2751.py
@@ -1,6 +1,6 @@
 """
 https://www.acmicpc.net/problem/2751
-
+수 정렬하기 2 / 실버 5 / 3분
 """
 import sys
 input = sys.stdin.readline
@@ -9,6 +9,7 @@ numbers = []
 for i in range(n):
     numbers.append(int(input()))
 
+# 내부 sort 이용(tim sort)
 numbers.sort()
 for i in numbers:
     print(i)
