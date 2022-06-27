50bb53681ffa29387e540d7bc417fa446ef27bbc
wlsgh7608 <wlsgh7608@naver.com>
2022-06-27 15:38:39 +0900

    2022-06-27

--- tree/baekjoon/1167.py
+++ tree/baekjoon/1167.py
@@ -1,41 +1,41 @@
 """
 https://www.acmicpc.net/problem/1167
-트리의 지름 / 골드 3 /
+트리의 지름 / 골드 2 /
 """
 import sys
 input = sys.stdin.readline
+from collections import deque
+
+
+
 n = int(input())
-G = [[] for _ in range(n)]
-
-def bfs(vertex):
-    from collections import deque
-    max_len = 0
-    visited = [False for _ in range(len(G))]
-    visited[vertex] = True
-    Q = deque()
-    Q.append((vertex,0))
-    while Q:
-        v,w = Q.popleft()
-        max_len = max(max_len,w)
-        for next,n_w in G[v]:
-            if not visited[next]:
-                visited[next] = True
-                Q.append((next,w+n_w))
-    return max_len
-
-
+G = [[] for _ in range(n+1)]
 for _ in range(n):
-    a, *next = map(int,input().split())
-    next = next[:-1]
-    i = 0
-    while i <len(next):
-        b,w = next[i],next[i+1]
-        G[a-1].append((b-1,w))
-        i+=2
+    start , *others  = map(int,input().split()) 
+    others = others[:-1]
+    for i in range(0,len(others),2):
+        G[start].append((others[i],others[i+1]))
+
+def bfs(node):
+    visited = [False for _ in range(n+1)]
+    Q = deque()
+    Q.append((0,node))
+    visited[node] = True
+    result = [0,0]
+    while Q:
+        dist , current = Q.popleft()
+        for next,extra_dist in G[current]:
+            if not visited[next]:
+                visited[next] =True
+                Q.append((dist+extra_dist,next))
+                if result[0] < dist+extra_dist:
+                    result[0] = dist+extra_dist
+                    result[1] = next
+    return result
+result = 0
+first_dist,first_node = bfs(1)
+second_dist,_ = bfs(first_node)
+result = max(result,first_dist,second_dist)
+print(second_dist)
 
 
-max_dist = 0
-for i in range(len(G)):
-    max_dist = max(max_dist,bfs(i))
-    
-print(max_dist)
